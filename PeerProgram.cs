78112f1453482e1c6ea9dbd25326ffd765021198
SomeoneElsHere <161262730+SomeoneElsHere@users.noreply.github.com>
2026-09-08 19:03:51 -0500

    Initial commit

---
+++ PeerProgram.cs
@@ -0,0 +1,16 @@
+﻿using System.Drawing;
+
+namespace NETWORK_2026
+{
+    public class PeerProgram
+    {
+        string stream;
+
+        public List<string> Divider(string s, int n)
+        {
+            List<string> strList;
+
+        }
+        
+    }
+}
